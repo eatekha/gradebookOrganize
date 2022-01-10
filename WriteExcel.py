582494834112271310ad2d32160ca8062c6bd24f
eatekha <77559961+eatekha@users.noreply.github.com>
2022-01-10 18:11:19 -0500

    Update WriteExcel.py

--- WriteExcel.py
+++ WriteExcel.py
@@ -2,103 +2,41 @@ from Report import *
 from openpyxl import *
 from openpyxl.styles import Font
 from openpyxl.worksheet.table import Table, TableStyleInfo
-from openpyxl.worksheet.dimensions import ColumnDimension
 import random
 
-""" Functions for Excel Worksheet """
-
-"""def autofitColumns():
-    letters = 'ABCDEFGHIJ'  # to add more
-    for x in range(len(letters)):
-        each_letter = letters[x]
-        ws.column_dimensions[each_letter].auto_size = True"""
-def makeTableHeaders():
-    letters = 'ABCDEFGHIJ'  # to add more
-    count = 0
-
-    if len(assigndict) > 0:
-        ws[str(letters[count]) + "8"] = "AssignmentName"
-        ws[str(letters[count + 1]) + "8"] = "AssignmentGrade"
-        count += 2
-    if len(quizdict) > 0:
-        ws[str(letters[count]) + "8"] = "QuizName"
-        ws[str(letters[count + 1]) + "8"] = "QuizGrade"
-        count += 2
-    if len(labdict) > 0:
-        ws[str(letters[count]) + "8"] = "LabName"
-        ws[str(letters[count + 1]) + "8"] = "LabGrade"
-        count += 2
-    if len(examdict) > 0:
-        ws[str(letters[count]) + "8"] = "ExamName"
-        ws[str(letters[count + 1]) + "8"] = "ExamGrade"
-
-    return letters[count + 1]
-
-
-def addToRows():
+def writeToExcel():
+    """Variables """
+    dictlist = [assigndict,quizdict,labdict,otherdict,examdict]
     letters = 'ABCDEFGHIJK'  # to add more
     l_count = 0
-    num_count = 9
-    max = 0
+    num_count = 8
+    max_letter = 'A',
+    max_num = 0
 
-    if assigndict:
-        for assignment in assigndict:
-            # bolding names
-            aName = ws[str(letters[l_count]) + str(num_count)]
-            aName.font = Font(bold=True)
-            aName.value = assignment
+    # Algorithm to add name and grade to excel sheet
+    for dict in dictlist:
+        if len(dict) > 1:  # if dictionary isn't empty
 
-            ws[str(letters[l_count + 1]) + str(num_count)] = assigndict[assignment]
-            num_count += 1
-        if num_count > max:
-            max = num_count
+            for key in dict:  # iterating through each dictionary
+                """NAMES"""
+                aName = ws[str(letters[l_count]) + str(num_count)]
+                aName.value = key
+                if num_count != 8:
+                    aName.font = Font(bold=True)
+                '''VALUES'''
+                ws[str(letters[l_count + 1]) + str(num_count)] = dict[key]
+                num_count += 1
 
-        num_count = 9
-        l_count += 2
+            max_letter = letters[l_count + 1]
+            """FINDING THE LAST CELL"""
+            if num_count > max_num:
+                max_num = num_count
+            '''Row count resets back to 8
+                skips to two columns over'''
+            num_count = 8
+            l_count += 2
 
-    if quizdict:
-        for quiz in quizdict:
-            aName = ws[str(letters[l_count]) + str(num_count)]
-            aName.font = Font(bold=True)
-            aName.value = quiz
-
-            ws[str(letters[l_count + 1]) + str(num_count)] = quizdict[quiz]
-            num_count += 1
-        if num_count > max:
-            max = num_count
-        num_count = 9
-        l_count += 2
-
-    if labdict:
-        num_count = 9
-        for lab in labdict:
-            aName = ws[str(letters[l_count]) + str(num_count)]
-            aName.font = Font(bold=True)
-            aName.value = lab
-
-            ws[str(letters[l_count + 1]) + str(num_count)] = labdict[lab]
-            num_count += 1
-        if num_count > max:
-            max = num_count
-
-        num_count = 9
-        l_count += 2
-
-    if examdict:
-        for exam in examdict:
-            aName = ws[str(letters[l_count]) + str(num_count)]
-            aName.font = Font(bold=True)
-            aName.value = exam
-
-            ws[str(letters[l_count + 1]) + str(num_count)] = examdict[exam]
-            num_count += 1
-        l_count += 2
-        if num_count > max:
-            max = num_count
-        l_count += 2
-
-    return str(max - 1)
-
+    return max_letter + str(max_num-1)
 
 """Iterating data into Excel table"""
 try:
@@ -133,17 +71,16 @@ try:
     title.value = "Grade Report for " + className
     title.font = Font(size=15,italic=True,bold=True, underline='single')
 
-    max_column = makeTableHeaders()  # makes the table headers
-    max_row = addToRows()
+    last_cell = writeToExcel()
 
     try:
         randomNum = random.randint(0, 10000)
         tab = Table(displayName="GradesTablev" + str(randomNum),
-                    ref="A8:" + max_column + max_row)  # calling header value calls last value form makingHeaders()
+                    ref="A8:" + last_cell)  # calling header value calls last value form makingHeaders()
     except ValueError:
         randomNum = random.randint(0, 10000)
         tab = Table(displayName="GradesTablev" + str(randomNum),
-                    ref="A8:" + max_column + max_row)
+                    ref="A8:" + last_cell)
 
     # Add a default style with striped rows and banded columns
     style = TableStyleInfo(name="TableStyleMedium9", showFirstColumn=False,
@@ -151,9 +88,11 @@ try:
     tab.tableStyleInfo = style
     ws.add_table(tab)
     wb.save(file + ".xlsx")
-    
+
     print("\nTo autofit width of table in Microsoft Excel, highlight all of the gradeTable, "
-          "click the FORMAT button on the top right, then click AUTOFIT COLUMN WIDTH.\n Enjoy!")
+      "click the FORMAT button on the top right, then click AUTOFIT COLUMN WIDTH.\n Enjoy!")
+
+
 except PermissionError:
     print("Unable to convert to Excel file. \nMake sure the workbook is closed and try again.")
 
@@ -162,7 +101,3 @@ except FileNotFoundError:
 
 except ValueError:
     print("Code Error. Please try again.")
-except:
-    print("UnknownError, please contact atekhaeseosa@gmail.com")
-
-
